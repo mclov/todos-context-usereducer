4ee55c70425f92ad720ab60eaae327be85b971b4
Michael Clovis <michaelclovis@protonmail.com>
2020-01-03 12:37:10 -0800

    Refactor with context hooks

---
+++ src/context/todos.context.js
@@ -0,0 +1,18 @@
+import React, { createContext } from 'react';
+import useTodoState from '../hooks/useTodoState';
+
+const defaultTodos = [
+  { id: 1, task: "Mow the lawn", completed: false },
+  { id: 2, task: "Release ladybugs into the garden", completed: true }
+];
+
+export const TodosContext = createContext();
+
+export function TodosProvider(props) {
+  const todosStuff = useTodoState(defaultTodos);
+  return (
+    <TodosContext.Provider value={todosStuff}>
+      {props.children}
+    </TodosContext.Provider>
+  )
+}
